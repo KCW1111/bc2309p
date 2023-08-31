646992f2ee82f3823639a2e9a017f7fa91fbaf36
kc <metaborism2@gmail.com>
2023-08-31 21:57:36 +0800

    2nd commit

---
+++ demo-datatype/src/DataType.java
@@ -0,0 +1,12 @@
+public class DataType{
+    public static void main(String arg[]){
+        //read the code from right to left
+        String box = "Hello World Me!!!"; //Assign value "Hello World" to a variable box
+        System.out.println(box);//Print a variable
+        System.out.println("box"); // Print the String Value: box
+
+        String str; // Declaration
+        str = "abc"; // Assign value "abc" to a variable str
+        System.out.println(str); // abc
+    }
+}
---
+++ demo-helloworld/src/Helloworld.java
@@ -0,0 +1,7 @@
+public class Helloworld {
+    // This is comment, I can type anythin here
+    public static void main(String[] args) {
+        System.out.println("EFGG Hello World! 你好！");
+
+    }
+}
